87711a3ea0b36521c2271dbb2872bdc0012f106c
Charles <charles.alexandre.meance@gmail.com>
2023-02-22 12:13:31 -0400

    Enable use of static files and rendering

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const PORT = 3000;
 
 const app = express();
 
+const publicRessourcesPath = path.join(__dirname, '..', 'public');
+const pathToViews = path.join(__dirname, '..', 'templates', 'views');
+
+app.set('views', pathToViews);
+
+app.use(express.static(publicRessourcesPath));
+app.set('view engine', 'hbs');
+
 app.get('', (req, res) => {
-    res.send('<h1>OK</h1>');
+    res.render('index');
 });
 
 app.listen(PORT, () => {
